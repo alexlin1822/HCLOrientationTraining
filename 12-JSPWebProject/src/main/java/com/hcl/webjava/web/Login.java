b4cb961e03ec1299babb3ac651b9a345d96ac0df
Alex Lin <alexlin1822@hotmail.com>
2022-02-14 13:00:44 -0500

    Added error message

--- 12-JSPWebProject/src/main/java/com/hcl/webjava/web/Login.java
+++ 12-JSPWebProject/src/main/java/com/hcl/webjava/web/Login.java
@@ -27,7 +27,7 @@ public class Login extends HttpServlet {
 				// redirects to successful.jsp when user name and password are correct.
 				resp.sendRedirect("successful.jsp");
 			} else {
-				resp.sendRedirect("index.jsp");
+				resp.sendRedirect("failure.jsp");
 			}
 		} catch (ClassNotFoundException e) {
 			System.out.print(e);
